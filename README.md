852513632a32e7c04b60b95dadca1d507073c4e9
ROODAY <rudytheninja@gmail.com>
2016-07-24 17:41:51 -0400

    mockup complete

---
+++ README.md
@@ -0,0 +1,6 @@
+NECIR Hackathon Webapp
+=====
+
+Webapp for the NECIR Hackathon on August 3rd. Allows users to view and edit data of campaign contributors.
+
+Made with the Google Material Design Lite framework and Google Firebase.
--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ var config = {
 	storageBucket: "",
 };
 firebase.initializeApp(config);
-
 var provider = new firebase.auth.GoogleAuthProvider();
 var database = firebase.database();
 
@@ -13,18 +12,28 @@ var landing = document.getElementById('landing');
 var landingLogin = document.getElementById('landing-login');
 var logout = document.getElementById('logout');
 var login = document.getElementById('login');
+var addEntryBtn = document.getElementById('add-entry');
 var snackbarContainer = document.querySelector('#necir-snackbar');
 var datalist = document.getElementById('data-list');
-var editButtons;
+var editButtons, deleteButtons;
 
 var dialog = document.querySelector('#dialog');
+var dialog2 = document.querySelector('#dialog2');
+
 if (! dialog.showModal) {
   dialogPolyfill.registerDialog(dialog);
+  dialogPolyfill.registerDialog(dialog2);
 }
+
 var dialogName = document.getElementById('entry-name');
 var dialogCharacter = document.getElementById('entry-character');
 var dialogDescription = document.getElementById('entry-description');
 var dialogSave = document.getElementById('save-entry');
+
+var dialog2Name = document.getElementById('entry-name2');
+var dialog2Character = document.getElementById('entry-character2');
+var dialog2Description = document.getElementById('entry-description2');
+var dialog2Save = document.getElementById('save-entry2');
 
 function hasClass(el, className) {
   if (el.classList)
@@ -47,44 +56,6 @@ function removeClass(el, className) {
     el.className=el.className.replace(reg, ' ')
   }
 }
-
-landingLogin.addEventListener('click', function() {
-	firebase.auth().signInWithPopup(provider).then(function(result) {
-	  // This gives you a Google Access Token. You can use it to access the Google API.
-	  var token = result.credential.accessToken;
-	  // The signed-in user info.
-	  var user = result.user;
-	  landing.style.margin = "-100vh";
-	  setTimeout(function(){
-	  	addClass(landing, 'hidden');
-	  }, 500);
-	  document.getElementById('user-name').innerHTML = user.displayName;
-	  document.getElementById('propic').src = user.photoURL;
-	  addClass(login, 'hidden');
-	  removeClass(logout, 'hidden');
-	  var snackbarData = {
-	    message: 'Login Successful',
-	    timeout: 2000
-	  };
-	  snackbarContainer.MaterialSnackbar.showSnackbar(snackbarData);
-	  console.log(user);
-	  reloadList();
-	}).catch(function(error) {
-	  // Handle Errors here.
-	  var errorCode = error.code;
-	  var errorMessage = error.message;
-	  // The email of the user's account used.
-	  var email = error.email;
-	  // The firebase.auth.AuthCredential type that was used.
-	  var credential = error.credential;
-	  var snackbarData = {
-	    message: 'Login Unsuccessful',
-	    timeout: 2000
-	  };
-	  snackbarContainer.MaterialSnackbar.showSnackbar(snackbarData);
-	  console.error(error);
-	});
-}, false);
 
 function firebaseLogin() {
 	firebase.auth().signInWithPopup(provider).then(function(result) {
@@ -142,6 +113,13 @@ function firebaseLogout() {
 	});
 }
 
+function addEntry() {
+	dialog2Name.innerHTML = "";
+	dialog2Character.innerHTML = "";
+	dialog2Description.innerHTML = "";
+	dialog2.showModal();
+}
+
 function editEntry() {
 	var path = this.getAttribute("data-path");
 	firebase.database().ref(path).once('value').then(function(snapshot) {
@@ -171,25 +149,116 @@ function saveEntry(path, entryName, entryCharacter, entryDescription) {
   reloadList();
 }
 
+function saveNewEntry(entryName, entryCharacter, entryDescription) {
+  var newActorKey = firebase.database().ref().child('actors').push().key;
+  var newActor = {
+    name: entryName,
+    character: entryCharacter,
+    description: entryDescription,
+    key: newActorKey
+  };
+  
+  var updates = {};
+  updates['/actors/' + newActorKey] = newActor;
+  firebase.database().ref().update(updates);
+
+  var snackbarData = {
+    message: 'Changes Submitted',
+    timeout: 2000
+  };
+  snackbarContainer.MaterialSnackbar.showSnackbar(snackbarData);
+
+  dialog2.close();
+  reloadList();
+}
+
+function deleteEntry() {
+	var path = this.getAttribute("data-path");
+	firebase.database().ref(path).set(null);
+	reloadList();
+
+	var snackbarData = {
+	  message: 'Actor Deleted',
+	  timeout: 2000
+	};
+	snackbarContainer.MaterialSnackbar.showSnackbar(snackbarData);
+}
+
 function reloadList() {
 	datalist.innerHTML = "";
-	firebase.database().ref('/test/actors').once('value').then(function(snapshot) {
-	var actors = snapshot.val();
-	  for (var i = 0; i < actors.length; i++) {
-		var li = document.createElement("li");
-		li.setAttribute("class", "mdl-list__item mdl-list__item--three-line");
-		li.innerHTML = '<span class="mdl-list__item-primary-content"><span>' + actors[i].name + ' AKA ' + actors[i].character + '</span><span class="mdl-list__item-text-body">' + actors[i].description + '</span></span><span class="mdl-list__item-secondary-content"><a data-path="/test/actors/' + i + '" class="mdl-list__item-secondary-action data-edit-btn" href="#"><i class="material-icons">mode_edit</i></a></span>';
-		datalist.appendChild(li);
+	firebase.database().ref('/actors').once('value').then(function(snapshot) {
+	  var actors = snapshot.val();
+	  for (var key in actors) {
+	  	  if (actors.hasOwnProperty(key)) {
+			  var li = document.createElement("li");
+			  li.setAttribute("class", "mdl-list__item mdl-list__item--three-line");
+			  li.innerHTML = '<span class="mdl-list__item-primary-content"><span>' + actors[key].name + ' AKA ' + actors[key].character + '</span><span class="mdl-list__item-text-body">' + actors[key].description + '</span></span><span class="mdl-list__item-secondary-content"><a id="a' + key + '" data-path="/actors/' + actors[key].key + '" class="mdl-list__item-secondary-action data-edit-btn" href="#"><i class="material-icons">mode_edit</i></a></span><div class="mdl-tooltip mdl-tooltip--top" for="a'+ key + '">Edit</div><a id="b' + key + '" data-path="/actors/' + actors[key].key + '" class="mdl-list__item-secondary-action data-delete-btn" href="#"><i class="material-icons">delete</i></a></span><div class="mdl-tooltip mdl-tooltip--top" for="b'+ key + '">Delete</div>';
+			  datalist.appendChild(li);
+		  }
 	  }
 	  editButtons = document.getElementsByClassName("data-edit-btn");
       for (var i = 0; i < editButtons.length; i++) {
   	    editButtons[i].addEventListener('click', editEntry, false);
+      }
+      deleteButtons = document.getElementsByClassName("data-delete-btn");
+      for (var i = 0; i < deleteButtons.length; i++) {
+  	    deleteButtons[i].addEventListener('click', deleteEntry, false);
       }
     });
 }
 
 login.addEventListener('click', firebaseLogin);
 logout.addEventListener('click', firebaseLogout);
+addEntryBtn.addEventListener('click', addEntry);
 dialogSave.addEventListener('click', function() {
 	saveEntry(dialogName.getAttribute("data-path"), dialogName.textContent, dialogCharacter.textContent, dialogDescription.textContent);
+}, false);
+dialog2Save.addEventListener('click', function() {
+	if (dialog2Name.textContent !== "" && dialog2Name.textContent !== "Name" && dialog2Character.textContent !== "" && dialog2Character.textContent !== "Character" && dialog2Description.textContent !== "" && dialog2Description.textContent !== "Description") {
+		saveNewEntry(dialog2Name.textContent, dialog2Character.textContent, dialog2Description.textContent);
+	} else {
+		var snackbarData = {
+		  message: 'One of the fields is empty or unchanged!',
+		  timeout: 2000
+		};
+		snackbarContainer.MaterialSnackbar.showSnackbar(snackbarData);
+	}
+}, false);
+
+landingLogin.addEventListener('click', function() {
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+	  // This gives you a Google Access Token. You can use it to access the Google API.
+	  var token = result.credential.accessToken;
+	  // The signed-in user info.
+	  var user = result.user;
+	  landing.style.margin = "-100vh";
+	  setTimeout(function(){
+	  	addClass(landing, 'hidden');
+	  }, 500);
+	  document.getElementById('user-name').innerHTML = user.displayName;
+	  document.getElementById('propic').src = user.photoURL;
+	  addClass(login, 'hidden');
+	  removeClass(logout, 'hidden');
+	  var snackbarData = {
+	    message: 'Login Successful',
+	    timeout: 2000
+	  };
+	  snackbarContainer.MaterialSnackbar.showSnackbar(snackbarData);
+	  console.log(user);
+	  reloadList();
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  var snackbarData = {
+	    message: 'Login Unsuccessful',
+	    timeout: 2000
+	  };
+	  snackbarContainer.MaterialSnackbar.showSnackbar(snackbarData);
+	  console.error(error);
+	});
 }, false);
