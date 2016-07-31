eca0bf6df231a451142f9fa7c8e0eb83a64117e0
ROODAY <rudytheninja@gmail.com>
2016-07-31 18:47:56 -0400

    test

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ firebase.initializeApp({
 });
 
 var db = firebase.database();
-var ref = db.ref("/unfilteredIndices");
+var ref = db.ref("/reports");
 
 var stdin = process.openStdin();
 console.log("Enter a Report ID: ");
